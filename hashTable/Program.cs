d7a284281518ecdf0dd1c392044bbbbe0882dec5
Gurpreet Singh <57633822+gurpreet36@users.noreply.github.com>
2021-09-07 18:00:01 +0530

    Hashtable

---
+++ hashTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+ 
+namespace hashtablepractice
+{
+    class Program
+    {
+        static void Main()
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add(1,"Karan");
+            hashtable.Add(2,"Das");
+            hashtable.Add(3,"Rahul");
+            hashtable.Add(4,"Gurpreet");
+ 
+ 
+            bool search(int a)
+            {
+                if(hashtable.Contains(a))
+                    return true;
+                else
+                    return false;
+            }
+ 
+            Console.WriteLine(search(2));
+ 
+        }
+    }
+}
